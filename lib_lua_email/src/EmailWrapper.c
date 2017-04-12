0fc343749da6a38e5ecdb7c6a9ed997fc723c651
Andrei Chelariu <andreichelariu92@yahoo.com>
2017-04-12 21:52:09 +0300

    Export IMAP structure to lua.

--- lib_lua_email/src/EmailWrapper.c
+++ lib_lua_email/src/EmailWrapper.c
@@ -4,6 +4,7 @@
 #include <lauxlib.h>
 
 #include "lib_email/src/SmtpConnection.h"
+#include "lib_email/src/ImapConnection.h"
 
 /**
  * \brief
@@ -145,19 +146,144 @@ static int deleteSmtp(lua_State* L)
     return 0;
 }
 
+/**
+ * \brief
+ * Function that creates an IMAP connection.
+ * Parameters received from the interpreter:
+ * type: true -> SSL false -> NO_SSL
+ * ip
+ * port
+ * user
+ * pass
+ * Returns a new imap connection or raises error.
+ */
+static int makeImap(lua_State* L)
+{
+    int imapSuccess = 0;
+    int isSsl = 0;
+    const char* ip = NULL;
+    int port = 0;
+    ImapConnection* connection = NULL;
+    
+    isSsl = lua_toboolean(L, 1);
+    
+    ip = lua_tolstring(L, 2, NULL);
+    imapSuccess = (ip != NULL);
+    
+    port = imapSuccess ? lua_tonumber(L, 3) : 0;    
+    imapSuccess = (port != 0);
+    
+    if (isSsl && imapSuccess) {
+        const char* user = NULL;
+        const char* pass = NULL;
+        
+        user = imapSuccess ? lua_tolstring(L, 4, NULL) : NULL;
+        imapSuccess = (user != NULL);
+        
+        pass = imapSuccess ? lua_tolstring(L, 5, NULL) : NULL;
+        imapSuccess = (pass != NULL);
+        
+        connection = imapSuccess ? ImapConnection_Create(IMAP_SSL, ip, port, user, pass) : NULL;
+        imapSuccess = (connection != NULL);
+    }
+    else if (imapSuccess) {
+        connection = imapSuccess ? ImapConnection_Create(IMAP_NO_SSL, ip, port) : NULL;
+        imapSuccess = (connection != NULL);
+    }
+    
+    if (imapSuccess) {
+        ImapConnection** connectionAddr = lua_newuserdata(L, sizeof(ImapConnection*));
+        imapSuccess = (connectionAddr != NULL);
+        
+        if (imapSuccess) {
+            *connectionAddr = connection;
+            luaL_getmetatable(L, "lua_email_imap");
+            lua_setmetatable(L, -2);
+        }
+    }
+    
+    if (imapSuccess) {
+        //TODO: Andrei: Find logging mechanism.
+        printf("IMAP connection created successfully ip=%s, port=%d\n", ip, port);
+        return 1;
+    }
+    else {
+        if (connection) {
+            ImapConnection_Delete(connection);
+        }
+        return luaL_error(L, "Error creating imap connection");
+    }
+}
+
+static int deleteImap(lua_State* L)
+{
+    ImapConnection** connectionAddr = NULL;
+    
+    connectionAddr = lua_touserdata(L, 1);
+    ImapConnection_Delete(*connectionAddr);
+    
+    //TODO: Andrei: find logging mechanism
+    printf("IMAP garbage collector called\n");
+    
+    return 0;
+}
+
+
+static int executeCommand(lua_State* L)
+{
+    ImapConnection** connectionAddr = NULL;
+    const char* command = NULL;
+    const char* output = NULL;
+    int commandSuccess = 0;
+    
+    connectionAddr = luaL_checkudata(L, 1, "lua_email_imap");
+    commandSuccess = (connectionAddr != NULL);
+   
+    command = commandSuccess ? lua_tolstring(L, 2, NULL) : NULL;
+    commandSuccess = (command != NULL);
+    
+    output = commandSuccess ? ImapConnection_ExecuteCommand(*connectionAddr, command) : NULL;
+    
+    if (commandSuccess && output) {
+        lua_pushstring(L, output);
+        return 1;
+    }
+    else if (commandSuccess && output == NULL) {
+        lua_pushnil(L);
+        lua_pushfstring(L, "%s command is not valid for this connection", command);
+        return 2;
+    }
+    else if (commandSuccess == 0) {
+        return luaL_error(L, "Error executing IMAP command");
+    }
+}
+
 static const luaL_Reg smtpFunctions[] = {
     {"sendEmail", sendEmail},
     {NULL, NULL}
 };
 
-static const luaL_Reg emailLibrary[] = {
-    {"makeSmtp", makeSmtp},
+static const luaL_Reg imapFunctions[] = {
+    {"executeCommand", executeCommand},
     {NULL, NULL}
 };
 
-int luaopen_lib_lua_email(lua_State* L)
+static const luaL_Reg emailLibrary[] = {
+    {"makeSmtp", makeSmtp},
+    {"makeImap", makeImap},
+    {NULL, NULL}
+};
+
+/**
+ * \brief
+ * Creates a metatable with the given name and sets itself as
+ * the __index field.
+ * Also, sets gcCallback as the __gc field.
+ * Leaves the metatable on top of the stack inside the given lua state.
+ */
+static void createMetatable(lua_State* L, const char* name, lua_CFunction gcCallback)
 {
-    luaL_newmetatable(L, "lua_email_smtp");
+    luaL_newmetatable(L, name);
     
     //set metatable as the index table for itself.
     //in this way, when a function is called for the userdate,
@@ -165,15 +291,25 @@ int luaopen_lib_lua_email(lua_State* L)
     lua_pushvalue(L, -1);
     lua_setfield(L, -2, "__index");
     
+    //insert gcCallback at the __gc index inside the metatable.
+    //this function will be called by the garbage collector when
+    //the user data needs to be removed.
     lua_pushstring(L, "__gc");
-    lua_pushcfunction(L, deleteSmtp);
+    lua_pushcfunction(L, gcCallback);
     lua_settable(L, -3);
-    
+}
+
+int luaopen_lib_lua_email(lua_State* L)
+{
+    createMetatable(L, "lua_email_smtp", deleteSmtp);    
     //put the smtp functions in the metatable
     luaL_setfuncs(L, smtpFunctions, 0);
     
+    createMetatable(L, "lua_email_imap", deleteImap);
+    luaL_setfuncs(L, imapFunctions, 0);
+    
     //register the emailLibrary functions in a table
-    //return the table to the interpreter.s
+    //return the table to the interpreter.
     luaL_newlib(L, emailLibrary);
     return 1;
 }
