445e24fc197012ac52b1eff3dd0e0b15dab1682b
Andrei Chelariu <andreichelariu92@yahoo.com>
2017-06-09 22:53:25 +0300

    Add parser function for SEARCH command. Use SELECT INBOX to find the last received email in the inbox.

--- app/email.lua
+++ app/email.lua
@@ -1,3 +1,4 @@
+--local dbg = require("debugger")
 email = require("lib_lua_email")
 
 email.ports = {
@@ -15,31 +16,16 @@ email.servers = {
 }
 
 email.imapCommands = {
-    searchLast= "SELECT INBOX",
     fetchHeader = function (id) return string.format("FETCH %d (BODY[HEADER.FIELDS (TO FROM SUBJECT)])", id) end,
     fetchBody = function (id) return string.format("FETCH %d BODY[TEXT]", id) end,
-    searchNew = "SEARCH NEW"
+    search = function (id) return string.format("SEARCH %d:*", id) end,
+    lastEmail = "SELECT INBOX"
 }
 
 email.imapParsers = {}
 
-function email.imapParsers.searchLast(response)    
-    local pattern = "%* (%d+) EXISTS"
-    local output = nil
-    local err = nil
+function email.imapParsers.fetch(header, body)
     
-    if response then
-        output = string.match(response, pattern)
-        output = output and (tonumber(output) or nil)
-    end
-    if not output then
-        err = "searchLast command has failed"
-    end
-    
-    return output, err
-end
-
-function email.imapParsers.fetch(header, body)    
     local output = nil
     local error = "INVALID header and body"
     
@@ -61,8 +47,8 @@ function email.imapParsers.fetch(header, body)
     
     if body then
         contentStart = string.find(body, "}")
-        contentStart = contentStart and ((contentStart + 1) or nil)
-        content = contentStart and (string.sub(body, contentStart) or nil)
+        contentStart = contentStart and (contentStart + 1) or nil
+        content = contentStart and string.sub(body, contentStart) or nil
     end
     
     if from and to and subject and content then
@@ -78,21 +64,31 @@ function email.imapParsers.fetch(header, body)
     return output, error
 end
 
-function email.imapParsers.searchNew(response)    
-    local id = nil
-    local error = "There is no new email in mailbox."
-    local pattern = "SEARCH (%d+)"
-    
-    if response then
-        id = string.match(response, pattern)
-        id = id and tonumber(id) or nil
+function email.imapParsers.search(response)        
+    local error = "There are no new emails in mailbox."
+    local list = {}
+    local pattern = "(%d+)"
+    for id in string.gmatch(response, pattern) do
+        if tonumber(id) then
+            table.insert(list, tonumber(id))
+            error = nil
+        end
     end
     
-    if id then
+    return list, error
+end
+
+function email.imapParsers.lastEmail(response)
+    local pattern = "* (%d+) EXISTS"
+    local output = nil
+    local error = "INVALID RESPONSE"
+    
+    output = string.match(response, pattern)
+    if output then
         error = nil
     end
-   
-    return id, error
+    
+    return output, error
 end
 
 local function runTests()
@@ -107,11 +103,7 @@ local function runTests()
     assert(email.servers.yahooSmtp == "smtps://smtp.mail.yahoo.com")
     assert(email.servers.gmailImap == "imaps://imap.gmail.com/INBOX")
     assert(email.servers.gmailSmtp == "smtps://smtp.gmail.com")
-    
-    --test simple imap commands
-    assert(email.imapCommands.searchLast == "SELECT INBOX")
-    assert(email.imapCommands.searchNew == "SEARCH NEW")
-    
+        
     local user = io.read()
     local pass = io.read()
     assert(user and pass)
@@ -121,26 +113,26 @@ local function runTests()
         user,
         pass)
     assert(connection)
-    
-    --test searchLast command
-    local command = email.imapCommands.searchLast
-    local response = connection:executeCommand(command)
-    local id, err = email.imapParsers.searchLast(response)
-    assert(id and not err)
-    
+        
     --test fetch command
-    command = email.imapCommands.fetchHeader(3)
-    local header = connection:executeCommand(command)
-    command = email.imapCommands.fetchBody(3)
+    command = email.imapCommands.fetchHeader(8)
+    local header, err = connection:executeCommand(command)
+    command = email.imapCommands.fetchBody(8)
     local body = connection:executeCommand(command)
     local e, err2 = email.imapParsers.fetch(header, body)
     assert(e.to and e.from and e.subject and e.content)
     
     --test search command
-    command = email.imapCommands.searchNew
+    command = email.imapCommands.search(3)    
     response, err2 = connection:executeCommand(command)
-    local success, err3 = email.imapParsers.searchNew(response)
-    assert(success or err3)
+    local list, err3 = email.imapParsers.search(response)
+    assert(#list or err3)
+    
+    --test lastEmail command
+    command = email.imapCommands.lastEmail
+    response, err = connection:executeCommand(command)
+    local idx = email.imapParsers.lastEmail(response)
+    assert(tonumber(idx))
         
     print("All tests went OK")
 end
